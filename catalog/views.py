edd3bfe15524114268c901df212ef73bd4748ff3
AlexandrBxbrxv <g1026@bk.ru>
2024-08-24 06:54:24 +0500

    + получение формы и вывод в консось

--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        user_name = request.POST.get('user_name')
+        user_email = request.POST.get('user_email')
+        user_message = request.POST.get('user_message')
+        print(user_name, user_email, user_message)
+    return render(request, 'catalog/contacts.html')
