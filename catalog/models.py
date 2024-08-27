f6c4538805ccef5c527e9fc1b4f969fdc19eb8a9
AlexandrBxbrxv <g1026@bk.ru>
2024-08-28 00:49:27 +0500

    связал модель Продукт с моделью Категория

--- catalog/models.py
+++ catalog/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
-
-class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='наименование')
-    description = models.TextField(blank=True, null=True, verbose_name='описание')
-    image = models.ImageField(blank=True, null=True, upload_to='product_images/', verbose_name='изображение')
-    category = models.CharField(max_length=150, verbose_name='категория')
-    price = models.IntegerField(verbose_name='цена')
-    created_at = models.DateField(verbose_name='дата создания')
-    changed_at = models.DateField(verbose_name='дата изменения')
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-        ordering = ('price',)
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
@@ -29,3 +13,22 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+    image = models.ImageField(**NULLABLE, upload_to='product_images/', verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='категория',
+                                 **NULLABLE, related_name='categories')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField(**NULLABLE, verbose_name='дата создания')
+    updated_at = models.DateField(**NULLABLE, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('price',)
