ca4b5cf2121d9a9413137e1d182635c0a94f6347
AlexandrBxbrxv <g1026@bk.ru>
2024-08-28 00:25:20 +0500

    + модель Продукт и Категория

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
+    image = models.ImageField(blank=True, null=True, upload_to='product_images/', verbose_name='изображение')
+    category = models.CharField(max_length=150, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField(verbose_name='дата создания')
+    changed_at = models.DateField(verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('price',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
