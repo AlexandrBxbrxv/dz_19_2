894b8d51d0abda733c0f536abbe873b189ea086b
AlexandrBxbrxv <g1026@bk.ru>
2024-09-05 05:24:40 +0500

    Добавил отображение Consumable, Equipment

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Consumable, Equipment
 
 
 @admin.register(Category)
@@ -8,8 +8,15 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
-@admin.register(Product)
+@admin.register(Consumable)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+@admin.register(Equipment)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'guarantee')
     list_filter = ('category',)
     search_fields = ('name',)
