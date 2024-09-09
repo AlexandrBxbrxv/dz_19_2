1314de2c2c347bb9453df7dae29d1cb41e4e1fa1
AlexandrBxbrxv <g1026@bk.ru>
2024-09-09 08:25:30 +0500

    + поле количество покупок товара для Техники и Расходников

--- catalog/models.py
+++ catalog/models.py
@@ -22,6 +22,7 @@ class Consumable(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='категория',
                                  **NULLABLE)
     price = models.IntegerField(verbose_name='цена')
+    purchases_count = models.IntegerField(default=0, verbose_name='единиц куплено')
     created_at = models.DateField(**NULLABLE, verbose_name='дата создания')
     updated_at = models.DateField(**NULLABLE, verbose_name='дата последнего изменения')
 
@@ -31,18 +32,19 @@ class Consumable(models.Model):
     class Meta:
         verbose_name = 'расходный материал'
         verbose_name_plural = 'расходные материалы'
-        ordering = ('price',)
+        ordering = ('price', 'purchases_count',)
 
 
 class Equipment(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
-    guarantee = models.IntegerField(default=0, verbose_name='гарантия')
+    guarantee = models.CharField(max_length=100, verbose_name='гарантия')
     manufacturer = models.CharField(max_length=200, verbose_name='производитель')
     description = models.TextField(**NULLABLE, verbose_name='описание')
     image = models.ImageField(**NULLABLE, upload_to='equipment/images', verbose_name='изображение')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='категория',
                                  **NULLABLE)
     price = models.IntegerField(verbose_name='цена')
+    purchases_count = models.IntegerField(default=0, verbose_name='единиц куплено')
     created_at = models.DateField(**NULLABLE, verbose_name='дата создания')
     updated_at = models.DateField(**NULLABLE, verbose_name='дата последнего изменения')
 
@@ -52,7 +54,7 @@ class Equipment(models.Model):
     class Meta:
         verbose_name = 'техника'
         verbose_name_plural = 'техника'
-        ordering = ('price',)
+        ordering = ('price', 'purchases_count',)
 
 
 class Blog(models.Model):
