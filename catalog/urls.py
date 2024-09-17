b48e1a44f7ba59a4339f8214a629acde177252a7
AlexandrBxbrxv <g1026@bk.ru>
2024-09-17 16:29:21 +0500

    расставил ссылки по порядку, привел их к единому виду

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,12 +9,13 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
     path('contacts', ContactsTemplateView.as_view(), name='contacts'),
+
+    path('consumable/create', ConsumableCreateView.as_view(), name='consumable_create'),
     path('consumables', ConsumablesListView.as_view(), name='consumables'),
+    path('consumable/detail/<int:pk>', ConsumableDetailView.as_view(), name='consumable_detail'),
+    path('consumable/update/<int:pk>', ConsumableUpdateView.as_view(), name='consumable_update'),
+    path('consumable/delete/<int:pk>', ConsumableDeleteView.as_view(), name='consumable_delete'),
+    path('consumable/purchase/<int:pk>', consumable_purchase_count, name='consumable_purchase'),
     path('equipments', EquipmentListView.as_view(), name='equipments'),
     path('equipment/detail/<int:pk>/', EquipmentDetailView.as_view(), name='equipment_detail'),
-    path('consumable/detail/<int:pk>/', ConsumableDetailView.as_view(), name='consumable_detail'),
-    path('consumable/create', ConsumableCreateView.as_view(), name='consumable_create'),
-    path('consumable/<int:pk>/update', ConsumableUpdateView.as_view(), name='consumable_update'),
-    path('consumable/<int:pk>/delete', ConsumableDeleteView.as_view(), name='consumable_delete'),
-    path('consumable/<int:pk>/purchase', consumable_purchase_count, name='consumable_purchase'),
 ]
